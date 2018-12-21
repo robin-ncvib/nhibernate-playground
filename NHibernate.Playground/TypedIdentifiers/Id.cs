f37faca56ce63257ed510cf2e84b9e1bffe8dea4
robin-ncvib <robin@ncvib.com>
2018-12-21 09:50:23 +0100

    Added a typed Id to be used later

---
+++ NHibernate.Playground/TypedIdentifiers/Id.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace NHibernate.Playground.TypedIdentifiers
+{
+    public class Id<T>
+    {
+        public Id()
+            : this(Guid.NewGuid())
+        {
+        }
+
+        public Id(Id<T> id)
+            : this(id.Value)
+        {
+        }
+
+        public Id(Guid id)
+        {
+            DomainType = typeof(T);
+            Value = id;
+        }
+
+        [XmlAttribute]
+        public Guid Value { get; set; }
+
+        [XmlIgnore]
+        public System.Type DomainType { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
---
+++ NHibernate.Playground/TypedIdentifiers/MeasurementPointId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using NHibernate.Playground.Domain;
+
+namespace NHibernate.Playground.TypedIdentifiers
+{
+    public class MeasurementPointId : Id<MeasurementPoint>
+    {
+        private MeasurementPointId() : base(Guid.NewGuid())
+        {
+        }
+
+        public static MeasurementPointId NewId()
+        {
+            return new MeasurementPointId();
+        }
+    }
+}
